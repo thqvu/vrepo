8c68b8b1dfc0028c12a48192286d429882f5f198
vuhqtran@gmail.com <vutran@Vus-MacBook-Air.local>
2020-07-19 21:18:23 +0700

    commit

--- Python/DevNet/exam01.py
+++ Python/DevNet/exam01.py
@@ -1,2 +1,6 @@
-print("Hi, my name is {}".format("Vu"))
-print("Hi, my name is {name}".format(name="Vu"))
+#print("Hi, my name is {}".format("Vu"))
+#print("Hi, my name is {name}".format(name="Vu"))
+#print("a b c".split(" "))
+#print(",".join(['a', 'b', 'c']))
+a = 128
+print(a.bit_length())
