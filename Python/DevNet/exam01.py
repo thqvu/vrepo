72efe3b36d3e201c3138a90327889c12f30c9c9b
Vu Tran <vuhqtran@gmail.com>
2021-01-05 14:41:06 +0700

    commit

--- Python/DevNet/exam01.py
+++
@@ -1,6 +0,0 @@
-#print("Hi, my name is {}".format("Vu"))
-#print("Hi, my name is {name}".format(name="Vu"))
-#print("a b c".split(" "))
-#print(",".join(['a', 'b', 'c']))
-a = 128
-print(a.bit_length())
