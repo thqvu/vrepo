6d9859e1618cab11e506cfb6df31da9b201f237e
Vu Tran <vuhqtran@gmail.com>
2021-10-09 07:11:27 +0700

    commit

--- code/codematplotlibvmenudrivenprogram/VePieChartSVSuDungNNLaptrinh.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-import numpy as np
-fig=plt.figure()
-ax=fig.add_axes([0,0,1,1])
-ax.axis('equal')
-langs=['GiaoVienTH', 'GiangvienDH', 'Mang-Dientu', 'LapTrinh']
-students=[12,2,3,4]
-ax.pie(students, labels=langs,autopct='%1.2f%%')
-plt.show()
