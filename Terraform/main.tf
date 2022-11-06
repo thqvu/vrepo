136bc8d45214efc0ac4acc54415da619833837d3
thqvu <vuhqtran@gmail.com>
2022-11-06 22:27:27 +0700

    commit

---
+++ Terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = ""
+  #Go to IAM to find access key & secret key
+  access_key = ""
+  secret_key = ""
+}
+
+# resource "<provider>_<resource_type>" "name" {
+#   config options...
+#   key = ""
+# }
